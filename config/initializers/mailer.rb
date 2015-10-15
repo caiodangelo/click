0a88e1481e639174f301ad9bc7bd5e24960ced48
Caio D'Angelo <caio.dangelo@m4u.com.br>
2015-10-15 15:01:47 -0300

    Mail settings

--- config/initializers/mailer.rb
+++ config/initializers/mailer.rb
@@ -12,12 +12,14 @@ end
 Rails.env.on(:production) do
   config.action_mailer.default_url_options = { host: 'http://www.clickhoras.com/' }
 
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: Rails.application.secrets.sendgrid_user_name,
-    password: Rails.application.secrets.sendgrid_password,
-    domain: 'sendgrid.me'
-  }
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true
+}
+
 end
